edf356d7dc360cb1f144742484c52206ff49f7ab
bgcode <violetcode@icloud.com>
2024-02-17 14:30:09 +0800

    s

--- getimg.sh
+++ getimg.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 file_folder=$(date "+%Y-%m-%d")
-threshold=$1
+# file_folder="2024-01-1"
+threshold=20
 if [ ! -d "$file_folder" ]; then
+  mkdir "$file_folder"
+else 
+  rm -rf  "$file_folder"
   mkdir "$file_folder"
 fi
 if [ ! -d img_name.txt ]; then
   touch img_name.txt
 fi
-geturl(){
+main(){
     url="https://www.dmoe.cc/random.php?return=json"
     links=()
-    counter=1
+    counter=0
     while [ $counter -le $threshold ]
     do
         response=$(curl -s "$url")
@@ -20,28 +24,12 @@ geturl(){
         # 检查 img_name.txt 中是否存在相同的文件名，如果不存在则添加链接
         if ! grep -q "$file_name" img_name.txt; then
             links+=("$link")
-            echo "$file_name"
+            wget -q --show-progress -P "$file_folder" -O "$file_folder/$file_name" -c "$link"
             echo "$file_name" >> img_name.txt  # 添加新的文件名到 img_name.txt 文件
         fi
         ((counter++))
     done
-    echo "${links[@]}"
 }
-download(){
-    wget -q --show-progress -P "$1" -O "$2" -c "$3"
-}
-creat(){
-  cdn_prefix="https://cdn.jsdelivr.net/gh/bgvioletsky/testaction"
-  find . -type f -iname "*.jpg" | awk 'length($0) >= 13' | jq --arg prefix "$cdn_prefix" -R -s  '[split("\n")[] | select(. != "") | sub("^\\."; "") | $prefix + .]' >> img_url.json
-}
-main(){
-  links=($(geturl))
-  for link in "${links[@]}"
-  do 
-    file_name=$(basename "$link")
-    download "$file_folder" "$file_folder/$file_name" "$link"
-  done
-  creat
-}
+
 main
 
