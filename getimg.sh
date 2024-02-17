278a79ceaf5dc94d5d34c2e01b39ff1186411b3c
bgcode <violetcode@icloud.com>
2024-02-17 11:50:26 +0800

    下载图片脚本

---
+++ getimg.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+file_folder=$(date "+%Y-%m-%d")
+threshold=$1
+if [ ! -d "$file_folder" ]; then
+  mkdir "$file_folder"
+fi
+if [ ! -d img_name.txt ]; then
+  wget 
+fi
+geturl(){
+    url="https://www.dmoe.cc/random.php?return=json"
+    links=()
+    counter=1
+    while [ $counter -le $1 ]
+    do
+        response=$(curl -s "$url")
+        link=$(echo "$response" | jq -r '.imgurl')
+        file_name=$(basename "$link")
+        # 检查 img_name.txt 中是否存在相同的文件名，如果不存在则添加链接
+        if ! grep -q "$file_name" img_name.txt; then
+            links+=("$link")
+            echo "$file_name"
+            echo "$file_name" >> img_name.txt  # 添加新的文件名到 img_name.txt 文件
+        fi
+        ((counter++))
+    done
+    echo "${links[@]}"
+}
+download(){
+    wget -q --show-progress -P "$1" -O "$2" -c "$3"
+}
+creat(){
+  cdn_prefix="https://cdn.jsdelivr.net/gh/bgvioletsky/testaction"
+  find . -type f -iname "*.jpg" | awk 'length($0) >= 13' | jq --arg prefix "$cdn_prefix" -R -s  '[split("\n")[] | select(. != "") | sub("^\\."; "") | $prefix + .]' > img_url.json
+}
+main(){
+  links=($(geturl "$threshold"))
+  for link in "${links[@]}"
+  do 
+    file_name=$(basename "$link")
+    download "$file_folder" "$file_folder/$file_name" "$link"
+  done
+  creat
+}
+main
+
